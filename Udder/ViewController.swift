d47c203aecf3f7b456cd5746d9a10a579630b01e
David Thuo <davidthuowork@gmail.com>
2021-04-29 00:35:18 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by David Thuo on 29/04/2021.
+//  Copyright © 2021 David Thuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
